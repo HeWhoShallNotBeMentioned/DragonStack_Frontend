7ef7c24413d4e75bc257e71118a4215121c48e89
Chris Underwood <cunderwoodmn@gmail.com>
2022-03-18 16:19:14 -0700

    update frontend package-lock.json and updated the address (ur) to local.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const BACKEND = {
-  ADDRESS: 'https://dragonstack-backend-api.herokuapp.com',
+  //ADDRESS: 'https://dragonstack-backend-api.herokuapp.com',
+  ADDRESS: 'http://localhost:3000',
 };
 
 export { BACKEND };
